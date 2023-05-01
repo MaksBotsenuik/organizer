3dac9338b0f2bae64cca8df9cab55930bdd3d862
Maksym <maksum.botsenuik@gmail.com>
2023-05-01 14:54:08 +0300

    main.py v = 1.0.0

--- main.py
+++ main.py
@@ -2,4 +2,17 @@ import os
 import keys as k
 
 if __name__ == "__main__":
-    print(os.listdir(path = '.'))
+    path = str(input())
+    files = os.listdir(path = path)
+    print(files)
+    for file in files:
+        if '.' in file:    
+            extention = file.split('.')
+            new_path = path+ '\\' + k.file_types_to_names[extention[-1]]
+            try:
+                os.mkdir(new_path)
+            except FileExistsError:
+                pass
+            os.rename(path + '\\' + file, new_path+'\\'+file)
+    files = os.listdir(path = path)
+    print(files)
